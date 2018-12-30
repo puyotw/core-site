5c32af89cea57ab5ea5404e25d2d9b3d5c2e9ad2
Hou In Si Tou <karbuncle@puyo.tw>
2018-12-29 17:50:24 -0800

    Fix double quote 3

--- _plugins/youtube.rb
+++ _plugins/youtube.rb
@@ -7,12 +7,12 @@ class YouTube < Liquid::Tag
     if markup =~ Syntax then
       @id = $1
     else
-      raise "No YouTube ID provided in the 'youtube' tag"
+      raise "No YouTube ID provided in the \"youtube\" tag"
     end
   end
 
   def render(context)
-    "<div class='video-container mb3'><iframe width='560' height='400' frameborder='0' allowfullscreen src='http://www.youtube.com/embed/#{@id}?color=white&theme=light&rel=0&amp;showinfo=0'></iframe></div>"
+    "<div class=\"video-container mb3\"><iframe width=\"560\" height=\"400\" frameborder=\"0\" allowfullscreen src=\"http://www.youtube.com/embed/#{@id}?color=white&theme=light&rel=0&amp;showinfo=0\"></iframe></div>"
   end
 
   Liquid::Template.register_tag "youtube", self
