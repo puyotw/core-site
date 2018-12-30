bab47519fc2557ed6ac0c77633f3b743c9cd83aa
Hou In Si Tou <git@hist.io>
2018-12-29 20:57:15 -0800

    fixup! 新增Hook以自動填充wiki collection頁面的category欄位。

--- _plugins/categorize_wiki_hook.rb
+++ _plugins/categorize_wiki_hook.rb
@@ -1,24 +1,24 @@
-require 'pathname';
+require 'pathname'
 
 # This hook adds subdirectories of the wiki collection as the categories of the
 # current wiki page. This assumes directory structure like _wiki/C1/C2/C3/page.md.
 Jekyll::Hooks.register :wiki, :post_init do |wiki|
-    # Choosing post_init hook because categories are typically populated
-    # during init time(refer to Jekyll Document class code).
-    # This is the soonest possible time to properly populate categories.
+  # Choosing post_init hook because categories are typically populated
+  # during init time(refer to Jekyll Document class code).
+  # This is the soonest possible time to properly populate categories.
 
-    # absolute path to _wiki directory on the system
-    collection_root = Pathname.new(wiki.collection.directory)
-    # absolute path to the currect wiki Markdown file
-    wiki_file_path = Pathname.new(wiki.path)
-    # Directories to be considered as categories.
-    # For example, in case of _wiki/C1/C2/C3/page.md, C1/C2/C3 should be considered.
-    #
-    # In case of _wiki/page.md, this yields ./ and therefore we need to exclude a
-    # category of '.' later; this also means such pages won't have categories.
-    category_dirs = wiki_file_path.relative_path_from(collection_root).dirname
+  # absolute path to _wiki directory on the system
+  collection_root = Pathname.new(wiki.collection.directory)
+  # absolute path to the currect wiki Markdown file
+  wiki_file_path = Pathname.new(wiki.path)
+  # Directories to be considered as categories.
+  # For example, in case of _wiki/C1/C2/C3/page.md, C1/C2/C3 should be considered.
+  #
+  # In case of _wiki/page.md, this yields ./ and therefore we need to exclude a
+  # category of '.' later; this also means such pages won't have categories.
+  category_dirs = wiki_file_path.relative_path_from(collection_root).dirname
 
-    wiki.data['categories'] = category_dirs.to_s
-                              .split(File::SEPARATOR)
-                              .reject do |dir| dir == '.' end
+  wiki.data['categories'] = category_dirs.to_s
+                            .split(File::SEPARATOR)
+                            .reject { |dir| dir == '.' }
 end
